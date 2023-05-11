8e5e5cac940fd98c873a492f108eab7616c312eb
Luis Antonio Martinez Marroquin <LuisMartinezMA12@gmail.com>
2023-05-10 23:49:53 -0600

    Todo funcional segunda version

--- expresionesRegulares.py
+++ expresionesRegulares.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify
-import re
+from flask import Flask, jsonify, request
 import pandas as pd
+import re
 
 app = Flask(__name__)
 
+# Lee el archivo de Excel y crea un DataFrame
 clientes_df = pd.read_excel('D:\OtroDc\Tareas\8cuatri\Compiladores\Analisador Lexico\ExpresionesRegulares\contactos.xlsx', engine='openpyxl')
+clientes_df = clientes_df.fillna("")  # Reemplaza todos los valores NaN con cadenas vacías
 clientes = clientes_df.to_dict('records')
 
 def buscar_clientes(palabra):
@@ -12,12 +14,6 @@ def buscar_clientes(palabra):
     pattern = re.compile(regex_pattern, re.IGNORECASE)
     coincidencias = [cliente for cliente in clientes if pattern.search(str(cliente["   Nombre Contacto "])) or pattern.search(str(cliente["Clave cliente"])) or pattern.search(str(cliente["Correo "])) or pattern.search(str(cliente["Teléfono Contacto  "]))]
     return coincidencias
-
-@app.route('/buscar', methods=['POST'])
-def post_buscar():
-    palabra = request.form.get('palabra')
-    coincidencias = buscar_clientes(palabra)
-    return jsonify(coincidencias)
 
 @app.route('/buscar/<string:palabra>', methods=['GET'])
 def get_buscar(palabra):
